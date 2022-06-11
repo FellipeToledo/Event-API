f2f279ae43e7e2b713d86b5281d7e35b9d4427d2
Fellipe <fellipetoledo.dev@gmail.com>
2022-06-11 12:02:09 -0300

    Initial commit

---
+++ src/main/java/com/toledo/eventapi/EventRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.toledo.eventapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toledo/eventapi/EventRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toledo.eventapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
