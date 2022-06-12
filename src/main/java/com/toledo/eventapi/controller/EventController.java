366ae757e99b36800a30dfd7a60604ce85724c97
Fellipe <fellipetoledo.dev@gmail.com>
2022-06-12 12:47:39 -0300

    implemented FindAll() method

--- src/main/java/com/toledo/eventapi/controller/EventController.java
+++ src/main/java/com/toledo/eventapi/controller/EventController.java
@@ -1,8 +1,14 @@
 package com.toledo.eventapi.controller;
 
+import com.toledo.eventapi.entity.Event;
+import com.toledo.eventapi.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author Fellipe Toledo
@@ -11,9 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EventController {
-    @RequestMapping("/")
+
+    @Autowired
+    private EventRepository eventRepository;
+    @RequestMapping("/api/events")
     @ResponseBody
-    public String Event(){
-        return "EVENT";
+    public List<Event> findAllEvents(){
+        return null;
     }
+
 }
