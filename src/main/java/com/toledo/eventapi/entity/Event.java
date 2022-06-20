c2edfad9cd226df2309cad5aad5054ced2bec133
Fellipe <fellipetoledo.dev@gmail.com>
2022-06-20 11:11:12 -0300

    Project Update

--- src/main/java/com/toledo/eventapi/entity/Event.java
+++ src/main/java/com/toledo/eventapi/entity/Event.java
@@ -1,16 +1,22 @@
 package com.toledo.eventapi.entity;
 
+import com.toledo.eventapi.enums.EventType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
 
 /**
  * @author Fellipe Toledo
@@ -25,24 +31,33 @@ import java.time.LocalDateTime;
 public class Event {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "id_cor")
-    private String idCor;
+    private LocalDateTime registerDateTime = LocalDateTime.now();
+
+    @Column(name = "event_type")
+    @Enumerated(EnumType.STRING)
+    private EventType eventType;
+
+    @Column(name = "id_source")
+    private String source;
 
     @Column(name = "starting_date_time")
     private LocalDateTime startingDateTime;
 
     @Column(name = "end_date_time")
     private LocalDateTime endDateTime;
-
+    
     private String location;
 
     private String description;
 
+    @Column(name = "blocked_roads")
+    private String blockedRoads;
+
     @Column(name = "impacted_lines")
-    private Integer impactedLines;
+    private String impactedLines;
 
     //private CimuReport cimuReport;
 
---
+++ src/main/java/com/toledo/eventapi/enums/EventType.java
@@ -0,0 +1,15 @@
+package com.toledo.eventapi.enums;
+
+/**
+ * @author Fellipe Toledo
+ * @created 16/06/2022
+ */
+public enum EventType {
+
+    OCCURRENCE("Ocorrência"),
+    PROGRAMMED("Programada");
+
+
+    EventType(String name) {
+    }
+}
